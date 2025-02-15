3938d95769edd8fd0e47d38eda22dcafcc53209b
Abilmansur-programmer <gidraisa77@gmail.com>
2025-02-15 15:42:58 +0500

    Первый коммит

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n[5];
+    for(int i=0;i<5;i++){
+        n[i]=i*3;
+    
+    cout<<n[i]<<" ";
+    }
+    return 0;
+
+}
+video_path = "rtsp://MainWave:MainWave2025@10.8.19.148:554/stream2"
